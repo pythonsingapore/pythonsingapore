8976275aa2b08758cf8caaf55b803b0397b6b165
Martin Brochhaus <mbrochh@gmail.com>
2012-07-30 19:32:48 +0800

    Replaced markdown plugin by version from pypi

--- scripts/show-memory.sh
+++ scripts/show-memory.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source $HOME/bin/script-settings-INSERT_PROJECTNAME.sh
+source $HOME/bin/script-settings-pysg.sh
 
 ps -u $USERNAME -o pid,rss,command | awk '{print $0}{sum+=$2} END {print "Total", sum}'
