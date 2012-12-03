57881e0da3c06ff3317ee315bb1fa7169531c566
Martin Brochhaus <mbrochh@gmail.com>
2012-12-03 21:33:21 +0800

    UPDATE YOUR VENV! Fixed breaking tests.
    We had some copy & pasted code from another project here that obviously didn't
    work :) Removed that junk. `fab test` now runs through.

--- website/webapps/django/myproject/user_data/tests/factories.py
+++ website/webapps/django/myproject/user_data/tests/factories.py
@@ -9,32 +9,3 @@ class UserProfileFactory(factory.Factory):
     FACTORY_FOR = models.UserProfile
 
     user = factory.SubFactory(UserFactory)
-
-
-class UndergraduateDegreeFactory(factory.Factory):
-    FACTORY_FOR = models.UndergraduateDegree
-
-    user = factory.SubFactory(UserFactory)
-    field_of_study = 'Field of study'
-
-
-class PersonalProfileFactory(factory.Factory):
-    FACTORY_FOR = models.PersonalProfile
-
-    user = factory.SubFactory(UserFactory)
-    last_name = 'Smith'
-    first_name = 'John'
-
-
-class PostgraduateDegreeFactory(factory.Factory):
-    FACTORY_FOR = models.PostgraduateDegree
-
-    user = factory.SubFactory(UserFactory)
-    field_of_study = 'Field of study'
-
-
-class WorkExperienceFactory(factory.Factory):
-    FACTORY_FOR = models.WorkExperience
-
-    user = factory.SubFactory(UserFactory)
-    job_title = 'Job title'
